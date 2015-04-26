54d00111315a60c54bc77ef8571c9f1dba2c1bfc
Giovanni Lodi <giovanni.lodi42@gmail.com>
2015-04-27 05:30:55 +1000

    Initial Commit

---
+++ expecta-plugins-example/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  expecta-plugins-example
+//
+//  Created by Giovanni on 27/04/2015.
+//  Copyright (c) 2015 mokacoding. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ expecta-plugins-example/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  expecta-plugins-example
+//
+//  Created by Giovanni on 27/04/2015.
+//  Copyright (c) 2015 mokacoding. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
