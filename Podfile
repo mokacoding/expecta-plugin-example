0cba8cdedb220cac92b678152ce01a4256227d26
Giovanni Lodi <giovanni.lodi42@gmail.com>
2015-04-27 06:01:15 +1000

    Add Expecta and Specta

---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '8.0'
+
+target 'expecta-plugins-example' do
+
+end
+
+target 'expecta-plugins-exampleTests', exclusive: true do
+  pod 'Specta'
+  pod 'Expecta'
+end
+
---
+++ Pods/Target Support Files/Pods-expecta-plugins-exampleTests-Expecta/Pods-expecta-plugins-exampleTests-Expecta-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_expecta_plugins_exampleTests_Expecta : NSObject
+@end
+@implementation PodsDummy_Pods_expecta_plugins_exampleTests_Expecta
+@end
