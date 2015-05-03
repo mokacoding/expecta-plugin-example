1d40bda42ea748ea20e86ed69b1036f4e0624864
Giovanni Lodi <giovanni.lodi42@gmail.com>
2015-05-03 16:14:01 +1000

    Add skeleton for custom matcher "isAFruit"

---
+++ expecta-plugins-exampleTests/EXPMatchers+isAFruit.h
@@ -0,0 +1,11 @@
+//
+//  EXPMatchers+isAFruit.h
+//  expecta-plugins-example
+//
+//  Created by Giovanni on 3/05/2015.
+//  Copyright (c) 2015 mokacoding. All rights reserved.
+//
+
+#import "Expecta.h"
+
+EXPMatcherInterface(isAFruit, (void))
---
+++ expecta-plugins-exampleTests/EXPMatchers+isAFruit.m
@@ -0,0 +1,14 @@
+//
+//  EXPMatchers+isAFruit.m
+//  expecta-plugins-example
+//
+//  Created by Giovanni on 3/05/2015.
+//  Copyright (c) 2015 mokacoding. All rights reserved.
+//
+
+#import "EXPMatchers+isAFruit.h"
+
+EXPMatcherImplementationBegin(isAFruit, (void)) {
+
+}
+EXPMatcherImplementationEnd
