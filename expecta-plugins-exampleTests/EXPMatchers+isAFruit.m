dbcb1102fa2f325ec4bad685d0a8c1bb80bfdc2c
Giovanni Lodi <giovanni.lodi42@gmail.com>
2015-05-03 16:20:38 +1000

    Add implementation for "isAFruit" matcher

--- expecta-plugins-exampleTests/EXPMatchers+isAFruit.m
+++ expecta-plugins-exampleTests/EXPMatchers+isAFruit.m
@@ -7,8 +7,31 @@
 //
 
 #import "EXPMatchers+isAFruit.h"
+#import "Fruit.h"
 
 EXPMatcherImplementationBegin(isAFruit, (void)) {
+    BOOL actualIsNil = actual == nil;
 
+    prerequisite(^BOOL {
+        return !actualIsNil;
+    });
+
+    match(^BOOL {
+        return [actual isKindOfClass:[Fruit class]];
+    });
+
+    failureMessageForTo(^NSString * {
+        if (actualIsNil) {
+            return @"the actual value in nil/null";
+        }
+        return [NSString stringWithFormat:@"expected: kind of %@, got: an instance of %@", [Fruit class], [actual class]];
+    });
+
+    failureMessageForNotTo(^NSString * {
+        if (actualIsNil) {
+            return @"the actual value in nil/null";
+        }
+        return [NSString stringWithFormat:@"expected: not kind of %@, got: an instance of %@", [Fruit class], [actual class]];
+    });
 }
 EXPMatcherImplementationEnd
